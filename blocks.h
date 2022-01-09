7f4b36e215aa65e55e6fec6514fb06c2dd7006f2
Axel <axesode+github@gmail.com>
2022-01-09 14:42:54 +0100

    added temp

--- blocks.h
+++ blocks.h
@@ -1,15 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/					/*Update Interval*/	/*Update Signal*/
+   /*Icon*/	/*Command*/ /*Update Interval*/	/*Update Signal*/
+   {"",  "sb-temp",        10,     6},      
    {"",  "sb-memory",      10,     6},      
    {"",  "sb-disk",        10,     6},      
    {"",  "sb-weather",     18000,  5},
-	{"",	"sb-volume",      1,      10},
+	 {"",	"sb-volume",       1,      10},
    {"",  "sb-bluetooth",   10,     13},
    {"",  "sb-backlight",   1,      12},
-	{"",	"sb-battery",     5,	     3},
-	{"",	"sb-internet",    5,	     4},
-	{"",	"sb-clock",       60,     0},
+	 {"",	"sb-battery",      5,	     3},
+	 {"",	"sb-internet",     5,	     4},
+	 {"",	"sb-clock",        60,     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.

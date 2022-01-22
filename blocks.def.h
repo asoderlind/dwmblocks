8f7008e60867e064ea6c1ef21ddee027c4d1b200
Axel <axesode+github@gmail.com>
2022-01-22 18:40:02 +0100

    updated blocks

--- blocks.def.h
+++ blocks.def.h
@@ -2,15 +2,16 @@
 static const Block blocks[] = {
    /*Icon*/	/*Command*/ /*Update Interval*/	/*Update Signal*/
    {"",  "sb-temp",        10,     6},      
+   {"",  "sb-cpu",         10,     6},      
    {"",  "sb-memory",      10,     6},      
    {"",  "sb-disk",        10,     6},      
-   {"",  "sb-weather",     18000,  5},
-	 {"",	"sb-volume",       1,      10},
-   {"",  "sb-bluetooth",   10,     13},
+ //{"",  "sb-weather",     18000,  5},
+	 {"",	 "sb-volume",      1,      10},
    {"",  "sb-backlight",   1,      12},
-	 {"",	"sb-battery",      5,	     3},
-	 {"",	"sb-internet",     5,	     4},
-	 {"",	"sb-clock",        60,     0},
+	 {"",	 "sb-battery",     5,	     3},
+   {"",  "sb-bluetooth",   10,     13},
+	 {"",	 "sb-internet",    5,	     4},
+	 {"",	 "sb-clock",       60,     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
